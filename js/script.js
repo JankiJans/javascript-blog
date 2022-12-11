f04bf7ca5a73d251cf78267c4760b5f30b785ccc
Jan Kędzierski <jkedzierski087@gmail.com>
2022-12-11 16:53:21 +0100

    blog version 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'; 
 
 /* const { active } = require("browser-sync"); */
 
@@ -17,7 +17,6 @@ const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event)
 
-  
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -54,10 +53,64 @@ const titleClickHandler = function(event){
 
     targetArticle.classList.add('active');
 
-  }
+}
   
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks (){
+
+    /* remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    console.log(titleList)
+
+    titleList.innerHTML = '';
+
+    /* for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for(let article of articles){
+
+        /* get the article id */
+
+        const articleId = article.getAttribute('id');
+
+        /* find the title element */
+
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+        /* get the title from the title element */
+
+
+
+        /* create HTML of the link */
+
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML)
+
+        /* insert link into titleList */
+
+        html = html + linkHTML;
+        console.log(html)
+
+    }
+
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links)
+}
+
+
+generateTitleLinks();
